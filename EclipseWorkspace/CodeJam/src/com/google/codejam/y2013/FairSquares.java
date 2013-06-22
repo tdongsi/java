9544ab2afb5f85b41f054f9c6ddce1cdf42098b0
Tue-Cuong Dong-Si <dongsi.tuecuong@gmail.com>
2013-06-22 01:45:26 -0700

    Google Code Jam 2013: Qualification round.

---
+++ EclipseWorkspace/CodeJam/src/com/google/codejam/y2013/FairSquares.java
@@ -0,0 +1,211 @@
+package com.google.codejam.y2013;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * TODO: this currently only solves the small and medium problems
+ * @author tdongsi
+ *
+ */
+public class FairSquares {
+
+	private static Logger logger = LoggerFactory.getLogger(FairSquares.class);
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+//		FairSquares solver = new FairSquares();
+//		solver.solve("data/FairSquaresSample.txt");
+//		solver.solve("C-small-attempt0.in");
+		
+		FairSquares solver = new FairSquares(1000000000000000L);
+		solver.solveQuick("data/FairSquaresSample.txt");
+//		solver.solveQuick("C-small-attempt0.in");
+//		solver.solveQuick("C-large-practice-1.in");
+	}
+	
+	/**
+	 * Empty constructor for solving small problem
+	 */
+	public FairSquares() {
+	}
+	
+	
+	/**
+	 * List of fair squares
+	 */
+	private List<Long> fairSquares;
+	
+	/**
+	 * Find all the fair squares during the interval 1 <= n <= upperBound
+	 * For solving medium problems
+	 * 
+	 * @param upperBound
+	 */
+	public FairSquares(long upperBound) {
+		fairSquares = new ArrayList<Long>(500);
+		
+		long i = 1;
+		long square = 1;
+		
+		while (square <= upperBound)
+		{
+			if ( isPalindrome(i) && isPalindrome(square) )
+			{
+				fairSquares.add(square);
+			}
+			
+			i++;
+			square = i*i;
+		}
+	}
+
+	private int testNum;
+	
+	/**
+	 * Find all the fair squares in the known interval and solve the test cases quickly
+	 * @param fileName
+	 */
+	public void solveQuick(String fileName)
+	{
+		try {
+			Scanner fileScanner = new Scanner(new File(fileName));
+			PrintStream out = new PrintStream("outputQuick.txt");
+			
+			// Processing input
+			String line;
+			line = fileScanner.nextLine();
+			testNum = Integer.valueOf(line);
+			
+			logger.debug("Case number: {}", testNum );
+			
+			for (int i = 0; i < testNum; i++) {
+				
+				Scanner lineScanner = new Scanner(fileScanner.nextLine());
+				// Get the input
+				long lowerBound = lineScanner.nextLong();
+				long upperBound = lineScanner.nextLong();
+				
+				logger.debug("Inputs: {} {}", lowerBound, upperBound);
+				
+				// Solve the problem
+				int count = 0;
+				for (int j = 0; j < fairSquares.size() && fairSquares.get(j) <= upperBound; j++ )
+				{
+					if ( fairSquares.get(j)  >= lowerBound )
+					{
+						count++;
+					}
+				}
+				String solution = Integer.toString(count);
+				
+				// Print the output
+				printSolution(i, solution, out);
+				printSolution(i, solution, System.out);
+			}
+			
+		} catch (FileNotFoundException e) {
+			System.err.println("File(s) not found");
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	/**
+	 * Solve the test cases one-by-one
+	 */
+	public void solve(String fileName)
+	{
+		try {
+			Scanner fileScanner = new Scanner(new File(fileName));
+			PrintStream out = new PrintStream("output.txt");
+			
+			// Processing input
+			String line;
+			line = fileScanner.nextLine();
+			testNum = Integer.valueOf(line);
+			
+			logger.debug("Case number: {}", testNum );
+			
+			for (int i = 0; i < testNum; i++) {
+				
+				Scanner lineScanner = new Scanner(fileScanner.nextLine());
+				// Get the input
+				long lowerBound = lineScanner.nextLong();
+				long upperBound = lineScanner.nextLong();
+				
+				logger.debug("Inputs: {} {}", lowerBound, upperBound);
+				
+				// Solve the problem
+				String solution = countFairSquares(lowerBound, upperBound);
+				
+				// Print the output
+				printSolution(i, solution, out);
+				printSolution(i, solution, System.out);
+			}
+			
+		} catch (FileNotFoundException e) {
+			System.err.println("File(s) not found");
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	/**
+	 * Count the number of palidromic squares between the lower bound and upper bound
+	 */
+	public String countFairSquares(long lowerBound, long upperBound) {
+		long startNum = (long) Math.ceil(Math.sqrt(lowerBound));
+		long i = startNum;
+		long square = i*i;
+		
+		int counter = 0;
+		
+		while (square <= upperBound)
+		{
+			if ( isPalindrome(i) && isPalindrome(square) )
+			{
+				logger.debug("Number: {}", square);
+				counter++;
+			}
+			
+			i++;
+			square = i*i;
+		}
+		
+		return Integer.toString(counter);
+	}
+
+	/**
+	 * Check if the given number is palidromic
+	 */
+	public boolean isPalindrome(long number) {
+		char[] string = Long.toString(number).toCharArray();
+		for (int i = 0; i < string.length/2; i++) {
+			if (string[i] != string[string.length-i-1])
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Print out the solution, either to console or to a file
+	 */
+	public void printSolution(int i, String solution, PrintStream outStream) {
+		outStream.println("Case #" + (i+1) + ": " + solution);
+	}
+
+}
---
+++ EclipseWorkspace/CodeJam/src/com/google/codejam/y2013/TicTacToe.java
@@ -0,0 +1,351 @@
+package com.google.codejam.y2013;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author tdongsi
+ *
+ */
+public class TicTacToe {
+
+	private static Logger logger = LoggerFactory.getLogger(TicTacToe.class);
+
+	public static void main(String[] args) {
+		TicTacToe solver = new TicTacToe();
+		solver.solve("data/TicTacToeSample.txt");
+//		solver.solve("A-small-attempt0.in");
+//		solver.solve("A-large.in");
+	}
+	
+	private int testNum;
+	private static final int SIZE = 4;
+	
+	private boolean isTableFull;
+
+	/**
+	 * Parsing the input data before calling the main method for solving
+	 */
+	public void solve(String fileName)
+	{
+		try {
+			Scanner fileScanner = new Scanner(new File(fileName));
+			PrintStream out = new PrintStream("output.txt");
+			
+			// Processing input
+			String line;
+			line = fileScanner.nextLine();
+			testNum = Integer.valueOf(line);
+			
+			for (int i = 0; i < testNum; i++) {
+				// Solve the problem
+				String solution = checkState(fileScanner);
+				// Skip the next empty line
+				line = fileScanner.nextLine();
+				
+				
+				// Print the output
+				printSolution(i, solution, out);
+				printSolution(i, solution, System.out);
+			}
+			
+			out.close();
+			
+		} catch (FileNotFoundException e) {
+			System.err.println("File(s) not found");
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	/**
+	 * Overload checkState to process input directly from file for speed
+	 * 
+	 * @param fileScanner
+	 * @return
+	 */
+	public String checkState(Scanner fileScanner) {
+		String statusMessage;
+		isTableFull = true;
+		List<Coordinate> xPos = new ArrayList<Coordinate>(SIZE*SIZE);
+		List<Coordinate> oPos = new ArrayList<Coordinate>(SIZE*SIZE);
+		
+		// process the input table
+		for (int i = 0; i < SIZE; i++) {
+			String line = fileScanner.nextLine();
+			
+			for (int j = 0; j < SIZE; j++) {
+				char currentChar = line.charAt(j);
+				
+				// Check if the table is full
+				if ( isTableFull && currentChar == '.')
+				{
+					isTableFull = false;
+				}
+				
+				if ( currentChar == 'X' || currentChar == 'T')
+				{
+					// position data for player X
+					Coordinate coord = new Coordinate(i, j);
+					xPos.add(coord);
+				}
+				
+				if ( currentChar == 'O' || currentChar == 'T')
+				{
+					// position data for player O
+					Coordinate coord = new Coordinate(i, j);
+					oPos.add(coord);
+				}
+			}
+		}
+		
+		// check the state
+		boolean isWinnerX = checkWinner(xPos);
+		boolean isWinnerO = checkWinner(oPos);
+		
+		if ( isWinnerX )
+		{
+			statusMessage = "X won";
+		} else if ( isWinnerO )
+		{
+			statusMessage = "O won";
+		} else if ( isTableFull )
+		{
+			statusMessage = "Draw";
+		} else
+		{
+			statusMessage = "Game has not completed";
+		}
+		
+		return statusMessage;
+	}
+
+	/**
+	 * Check if the player is winner, given the positions of his symbols
+	 * 
+	 * @param listPos
+	 * @return
+	 */
+	private boolean checkWinner(List<Coordinate> listPos) {
+		boolean checkColumn = checkWinnerColumn(listPos);
+		boolean checkRow = checkWinnerRow(listPos);
+		boolean checkDiagonal = checkWinnerDiagonal(listPos);
+		return (checkColumn || checkRow || checkDiagonal);
+	}
+
+	/**
+	 * Check if the player is winner by diagonal, given the positions of his symbols.
+	 * 
+	 * @param listPos
+	 * @return
+	 */
+	private boolean checkWinnerDiagonal(List<Coordinate> listPos) {
+		boolean checkDiagonal = true;
+		for (int i = 0; i < SIZE; i++) {
+			if ( !listPos.contains(new Coordinate(i,i)))
+			{
+				checkDiagonal = false;
+				break;
+			}
+		}
+		
+		boolean checkAntiDiagonal = true;
+		for (int i = 0; i < SIZE; i++) {
+			if ( !listPos.contains(new Coordinate(i,SIZE-i-1)))
+			{
+				checkAntiDiagonal = false;
+				break;
+			}
+		}
+		
+		return (checkDiagonal || checkAntiDiagonal);
+	}
+
+	/**
+	 * Check if the player is winner by row, given the positions of his symbols.
+	 * 
+	 * @param listPos
+	 * @return
+	 */
+	private boolean checkWinnerRow(List<Coordinate> listPos) {
+		int[] count = new int[SIZE];
+
+		for (Coordinate coordinate : listPos) {
+			count[coordinate.x]++;
+		}
+
+		for (int i = 0; i < count.length; i++) {
+			if ( count[i] == SIZE )
+				return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Check if the player is winner by column, given the positions of his symbols.
+	 * 
+	 * @param listPos
+	 * @return
+	 */
+	private boolean checkWinnerColumn(List<Coordinate> listPos) {
+		int[] count = new int[SIZE];
+		
+		for (Coordinate coordinate : listPos) {
+			count[coordinate.y]++;
+		}
+		
+		for (int i = 0; i < count.length; i++) {
+			if ( count[i] == SIZE )
+				return true;
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Check the status of the game, given the board description as 2D char array
+	 * 
+	 * @param table
+	 * @return
+	 */
+	private String checkState(char[][] table) {
+		String statusMessage;
+		isTableFull = true;
+		List<Coordinate> xPos = new ArrayList<Coordinate>(SIZE*SIZE);
+		List<Coordinate> oPos = new ArrayList<Coordinate>(SIZE*SIZE);
+		
+		// process the input table
+		for (int i = 0; i < SIZE; i++) {
+			for (int j = 0; j < SIZE; j++) {
+				// Check if the table is full
+				if ( isTableFull && table[i][j] == '.')
+				{
+					isTableFull = false;
+				}
+				
+				if ( table[i][j] == 'X' || table[i][j] == 'T')
+				{
+					// position data for player X
+					Coordinate coord = new Coordinate(i, j);
+					xPos.add(coord);
+				}
+				
+				if ( table[i][j] == 'O' || table[i][j] == 'T')
+				{
+					// position data for player O
+					Coordinate coord = new Coordinate(i, j);
+					oPos.add(coord);
+				}
+			}
+		}
+		
+		// check the state
+		boolean isWinnerX = checkWinner(xPos);
+		boolean isWinnerO = checkWinner(oPos);
+		
+		if ( isWinnerX )
+		{
+			statusMessage = "X won";
+		} else if ( isWinnerO )
+		{
+			statusMessage = "O won";
+		} else if ( isTableFull )
+		{
+			statusMessage = "Draw";
+		} else
+		{
+			statusMessage = "Game has not completed";
+		}
+		
+		return statusMessage;
+	}
+
+	/**
+	 * Construct the board description as 2D char array from the input file
+	 * 
+	 * @param fileScanner
+	 * @return
+	 */
+	private char[][] getTableFromFile(Scanner fileScanner) {
+		char[][] table = new char[SIZE][SIZE];
+		
+		for (int i = 0; i < SIZE; i++) {
+			String line = fileScanner.nextLine();
+			
+			for (int j = 0; j < SIZE; j++) {
+				table[i][j] = line.charAt(j);
+			}
+		}
+		
+		return table;
+	}
+
+	/**
+	 * Print out the solution, either to console or to a file
+	 * 
+	 * @param i
+	 * @param solution
+	 * @param outStream
+	 */
+	public void printSolution(int i, String solution, PrintStream outStream) {
+		outStream.println("Case #" + (i+1) + ": " + solution);
+	}
+
+}
+
+/**
+ * Simple class for a pair of numbers: (x,y)
+ * @author tdongsi
+ *
+ */
+class Coordinate
+{
+	public int x;
+	public int y;
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Coordinate other = (Coordinate) obj;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
+	
+	/**
+	 * @param x
+	 * @param y
+	 */
+	public Coordinate(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	
+	
+}
