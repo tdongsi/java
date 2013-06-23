2eb619bc5a20b3c3f89daa137f6265cf8d6cdd45
Tue-Cuong Dong-Si <dongsi.tuecuong@gmail.com>
2013-06-23 02:16:06 -0700

    Project Euler: data files for the last commit, problem 18. Solutions to problem 19 and 21.

---
+++ net.projecteuler/src/net/projecteuler/problems/Problem0019.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package net.projecteuler.problems;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+
+/**
+ * How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+ * 
+ * @author dongsi.tuecuong@gmail.com
+ *
+ */
+public class Problem0019 {
+
+	public static void main(String[] args) {
+		
+		Calendar calendar = new GregorianCalendar();
+		
+		// Sanity check
+		// The number of MONTH is 0-based. March is 2.
+		calendar.set(2013, 2, 02);
+		System.out.println("DAY_OF_WEEK: " + calendar.get(Calendar.DAY_OF_WEEK));
+		System.out.println("Sunday: " + Calendar.SUNDAY );
+		
+		int count = 0;
+		// From year 1901 to 2000.
+		for ( int i=1901; i <= 2000; i++ )
+		{
+			// From month January to December
+			for ( int j = 0; j < 12; j++ )
+			{
+				// Set to the first day of the month
+				calendar.set(i, j, 1);
+				
+				// If the date is Sunday
+				if ( calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY )
+				{
+					count++;
+				}
+			}
+		}
+		
+		System.out.println("The number of Sundays is " + count );
+
+	}
+
+}
---
+++ net.projecteuler/src/net/projecteuler/problems/Problem0021.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package net.projecteuler.problems;
+
+import java.util.List;
+
+import net.projecteuler.utility.BitSetPrimeSieve;
+import net.projecteuler.utility.NumberUtility;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
+ * If d(a) = b and d(b) = a, where a is not b, then a and b are an amicable pair and each of a and b are called amicable numbers.
+ * Evaluate the sum of all the amicable numbers under 10000.
+ * 
+ * @author dongsi.tuecuong@gmail.com
+ *
+ */
+public class Problem0021 {
+	
+	private static Logger logger = LoggerFactory.getLogger(Problem0021.class);
+
+	public static void main(String[] args) {
+		
+		Problem0021 solver = new Problem0021();
+		
+		solver.run(logger);
+	}
+
+	public void run(Logger logger2) {
+		
+		int maxNumber = 10000;
+		
+		int[] divisorSums = new int[maxNumber];
+		
+		BitSetPrimeSieve sieve = new BitSetPrimeSieve(maxNumber);
+		List<Integer> primes = sieve.getList();
+		
+		for (int i = 4; i < divisorSums.length; i++) {
+			// Primes cannot be amicable numbers
+			if ( !primes.contains(i))
+			{		
+				divisorSums[i] = (int)NumberUtility.computeProperDivsiorSum(i);
+				logger.debug("Number: {}", divisorSums[i] );
+			}
+		}
+		
+		int sum = 0;
+		for (int i = 4; i < divisorSums.length; i++) {
+			if ( !primes.contains(i))
+			{
+				int j = divisorSums[i];
+				
+				if ( j < divisorSums.length && divisorSums[j] == i && i != j )
+				{
+					logger.debug( "Pair of amicable numbers: {} {}", i, j );
+					sum += i;
+				}
+			}
+		}
+		
+		logger.info("Sum of amicable numbers is {}", sum );
+		
+	}
+
+}
