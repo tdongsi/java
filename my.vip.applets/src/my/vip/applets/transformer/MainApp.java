4c519ddccd102304429736e86ec65cf5e5b2367f
Tue-Cuong Dong-Si <dongsi.tuecuong@gmail.com>
2013-06-26 03:11:53 -0700

    Add a MainApp for AC Transformer applet, for easy running.

---
+++ my.vip.applets/src/my/vip/applets/transformer/MainApp.java
@@ -0,0 +1,37 @@
+package my.vip.applets.transformer;
+
+import javax.swing.JFrame;
+
+/**
+ * A TA project for the course Basic Electrical Enginering, under Vacation Internship Program (VIP).
+ * An applet to illustrate how a transformer works. Programmed in Java 1.4. 
+ * 
+ * @author dongsi.tuecuong@gmail.com
+ *
+ */
+public class MainApp {
+
+	/**
+	 * Run this applet as an application
+	 */
+	public static void main(String args[]) {
+		// Size of the application window
+		int width = 420, height = 422;
+
+		// Create the window container
+		JFrame window = new JFrame("Transformer demonstration");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// Create and start the main applet
+		Transformer applet = new Transformer();
+		applet.init();
+		applet.start();
+		
+		// Add the applet into the window frame container
+		window.getContentPane().add(applet);
+		window.setResizable(false);
+		window.setSize(width, height);
+		window.setVisible(true);
+	}
+
+}
