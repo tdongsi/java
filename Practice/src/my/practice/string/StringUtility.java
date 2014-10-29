957ebc5d8db431bbe1b775b6d8377458291c2929
Tue-Cuong Dong-Si <dongsi.tuecuong@gmail.com>
2014-10-29 01:48:33 -0700

    Practice with string manipulation exercises.

---
+++ Practice/src/my/practice/string/StringUtility.java
@@ -0,0 +1,10 @@
+package my.practice.string;
+
+public class StringUtility {
+	
+	// Assume that the number string is in range and valid
+	public static int atoi(String num) {
+		return 0;
+	}
+
+}
---
+++ Practice/test/my/practice/string/StringUtilityTest.java
@@ -0,0 +1,16 @@
+package my.practice.string;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringUtilityTest {
+
+	@Test
+	public void test_atoi() {
+		assertEquals(123, StringUtility.atoi("123"));
+		assertEquals(-123, StringUtility.atoi("-123"));
+		assertEquals(0, StringUtility.atoi("0"));
+	}
+
+}
