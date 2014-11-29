02c9dd229d4ca897f52a364b6d474c5f372d260e
Tue-Cuong Dong-Si <dongsi.tuecuong@gmail.com>
2014-11-29 01:02:51 -0800

    Add project summary to MainApp class and implementation Filters class.

--- my.vip.applets/src/my/vip/applets/filters/Filters.java
+++ my.vip.applets/src/my/vip/applets/filters/Filters.java
@@ -28,6 +28,12 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 /**
+ * In this implementation, the true effect of the passive analog filters in practical applications will be demonstrated. 
+ * In practice, most analog signals will come as arbitrary (periodic) waves, instead of sinusoidal waves. 
+ * We demonstrate that by examining the effect of different passive analog filters on different periodic waves, such as square and sawtooth waves. 
+ * Additionally, we examine the additive synthesis of periodic waves with a number of sinusoidal harmonics. 
+ * We also demonstrate the effect of the analog filters on the input wave as addition of effects on individual sinusoidal harmonics.
+ * 
  * @author tdongsi
  *
  */
