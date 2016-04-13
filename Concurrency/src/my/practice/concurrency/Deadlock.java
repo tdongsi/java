d882b6efe356953cf818c75e5add7c1508c0ac77
tdongsi <dongsi.tuecuong@gmail.com>
2016-04-12 18:12:25 -0700

    Place holder.

---
+++ Concurrency/src/my/practice/concurrency/Deadlock.java
@@ -0,0 +1,25 @@
+package my.practice.concurrency;
+
+/**
+ * Demonstrate a situation of deadlock.
+ * Two persons are high-fiving each other.
+ * The norm is one raises his hand and wait to receive 
+ * the other's high-five. The deadlock happens when both raise their
+ * hands and wait to receive the other's high-five.
+ * (Yeah, assume both are socially awkward)
+ * 
+ * @author cdongsi
+ *
+ */
+public class Deadlock {
+	
+	public static class HighFiver {
+		// TODO
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
