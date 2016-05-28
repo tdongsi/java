d0fb6c5666b395a6840a66b909450a5aab296944
tdongsi <dongsi.tuecuong@gmail.com>
2016-05-27 23:56:20 -0700

    Practice: Maximal Subarray.

--- Practice/src/my/practice/y2016/Practice.java
+++ Practice/src/my/practice/y2016/Practice.java
@@ -1,11 +1,38 @@
 package my.practice.y2016;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Practice {
 	
 	public static List<Integer> maximalSubarray(List<Integer> list) {
-		return list;
+		int sum = 0;
+		List<Integer> cumSum = new ArrayList<Integer>(list.size());
+		for (int elem : list) {
+			sum += elem;
+			cumSum.add(sum);
+		}
+		
+		int lowest = Integer.MAX_VALUE;
+		int profit = Integer.MIN_VALUE;
+		int lowestIdx = -1;
+		int profitIdx = -1;
+		int lowForProfitIdx = -1;
+		for (int i = 0; i < cumSum.size(); i++) {
+			int value = cumSum.get(i);
+			if (value < lowest) {
+				lowest = value;
+				lowestIdx = i;
+			}
+			
+			if (value - lowest > profit) {
+				profit = value - lowest;
+				profitIdx = i;
+				lowForProfitIdx = lowestIdx;
+			}
+		}
+		
+		return new ArrayList<Integer>(list.subList(lowForProfitIdx+1, profitIdx+1));
 	}
 
 }
