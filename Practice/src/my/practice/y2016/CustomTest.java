5ee9177ba581fac4e4f2c94d4765b768ad984c9d
tdongsi <dongsi.tuecuong@gmail.com>
2016-05-31 00:15:41 -0700

    Practice: Review reflection. Simple JUnit-like test runner.

--- Practice/src/my/practice/y2016/CustomTest.java
+++ Practice/src/my/practice/y2016/CustomTest.java
@@ -1,8 +1,11 @@
 package my.practice.y2016;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface CustomTest {
 	String author();
--- Practice/test/my/practice/y2016/TestRunner.java
+++ Practice/test/my/practice/y2016/TestRunner.java
@@ -1,5 +1,6 @@
 package my.practice.y2016;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class TestRunner {
@@ -10,17 +11,38 @@ public class TestRunner {
 		
 		try {
 			Class<?> c = Class.forName(ARG);
+			Object t = c.newInstance();
 			System.out.println("Running test class: "+ c.getName());
 			
 			Method[] methods = c.getMethods();
 			for (Method method : methods) {
 				if (method.getDeclaringClass() == c) {
-					System.out.println("Running: " + method.getName());
+					
+					// Only run methods with @CustomTest annotation
+					CustomTest ann = method.getAnnotation(CustomTest.class);
+					if ( ann != null ) {
+						System.out.println("Running: " + method.getName());
+						System.out.println("Author: " + ann.author() + " Version: " + ann.version().toString() );
+						method.invoke(t);
+					}
+					
 				}
 			}
 			
 		} catch (ClassNotFoundException e) {
 			System.out.println("Test Runner could not find class");
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
 		
